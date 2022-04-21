f32fd8b85d8fb04c28b8be1445749fd9d63169f2
Julian <Julian@DESKTOP-PFOCKI6>
2022-04-21 19:16:01 -0300

    Merge branch 'main' of https://github.com/JulianVillarroel/unqui-po2-villarroel.git into main

---
+++ test/ar/edu/unq/po2/tp3/EJ11EquipoDeTrabajo/EquipoDeTrabjoTestCase.java
@@ -0,0 +1,14 @@
+package ar.edu.unq.po2.tp3.EJ11EquipoDeTrabajo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EquipoDeTrabjoTestCase {
+
+	@Test
+	public void darNombreDelEquipoDeTrabajo() {
+		
+	}
+
+}
--- test/ar/edu/unq/po2/tp3/Ej1Contadores/CounterTestCase.java
+++ test/ar/edu/unq/po2/tp3/Ej1Contadores/CounterTestCase.java
@@ -1,9 +1,11 @@
-package ar.edu.unq.po2.tp3;
+package ar.edu.unq.po2.tp3.Ej1Contadores;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import ar.edu.unq.po2.tp3.Counter;
 
 public class CounterTestCase {
 private Counter counter;

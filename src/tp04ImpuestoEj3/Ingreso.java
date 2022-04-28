95f4436c835de873f84cd7b7e7a6dcd319181a5d
Julian <Julian@DESKTOP-PFOCKI6>
2022-04-28 19:22:40 -0300

    Impuestos ended whit test

--- src/tp04ImpuestoEj3/Ingreso.java
+++ src/tp04ImpuestoEj3/Ingreso.java
@@ -8,22 +8,25 @@ public class Ingreso {
 	private Month mesDePercepcion;
 	private String concepto;
 
-	public Ingreso(Month mesDePercepcion, String concepto, Integer montoPercibido) {
+	public Ingreso(Month mes, String concepto, Integer monto) {
 		this.mesDePercepcion = mes;
 		this.concepto = concepto;
 		this.montoPercibido = monto;
 	}
-	
+
 	public Month getMesDePercepcion() {
 		return mesDePercepcion;
 	}
-	
+
 	public String getConcepto() {
 		return concepto;
 	}
-	
+
 	public Integer getMontoPercibido() {
-		return monto;
+		return montoPercibido;
 	}
-	
+
+	public Integer getMontoImponible() {
+		return montoPercibido;
+	}
 }
